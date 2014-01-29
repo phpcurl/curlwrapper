0f2424ae77156ed3d10a5edb6fd63e9c2e857c79
Alexey Karapetov <karapetov@gmail.com>
2014-01-29 14:16:24 -0800

    formatting

--- autoload.php
+++ autoload.php
@@ -1,7 +1,7 @@
 <?php
 spl_autoload_register(function($class) {
-	$file = __DIR__.'/src/'.str_replace('\\', '/', $class).'.php';
-	if (is_file($file)) {
-		require_once($file);
-	}
+    $file = __DIR__.'/src/'.str_replace('\\', '/', $class).'.php';
+    if (is_file($file)) {
+        require_once($file);
+    }
 });
